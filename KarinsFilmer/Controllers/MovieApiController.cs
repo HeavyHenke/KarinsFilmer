40e4fec6cf71548287d86d455213d46796941007
Karin Langman <karinlangman@gmail.com>
2015-07-31 23:38:23 +0200

    Added ApiController and Get and Post Actions // Karin

---
+++ KarinsFilmer/Controllers/MovieApiController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace KarinsFilmer.Controllers
+{
+    public class MovieApiController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        public List<MovieTip> GetMovieTips()
+        {
+            return new List<MovieTip> {
+                new MovieTip { Title = "Django Unchained", Year = "2012", ImdbId = "tt1853728", Type = "movie" },
+                new MovieTip {Title = "Django Unchained 2", Year = "2012", ImdbId = "tt1853728", Type = "movie" },
+                new MovieTip {Title = "Hercules Unchained", Year = "1959", ImdbId = "tt0052782", Type = "movie" },
+                new MovieTip {Title = "America Unchained", Year = "2007", ImdbId = "tt1153075", Type = "movie" },
+                new MovieTip {Title = "Angel Unchained", Year = "1970", ImdbId = "tt0065401", Type = "movie" },
+                new MovieTip {Title = "Unchained Memories: Readings from the Slave Narratives", Year = "2003", ImdbId = "tt0343129", Type = "movie" },
+                new MovieTip {Title = "Women Unchained", Year = "1974", ImdbId = "tt0204742", Type = "movie" },
+                new MovieTip {Title = "Conan Unchained: The Making of 'Conan'", Year = "2000", ImdbId = "tt0430950", Type = "movie" },
+                new MovieTip {Title = "Unchained", Year = "1955", ImdbId = "tt0048762", Type = "movie" },
+                new MovieTip {Title = "The Unchained Goddess", Year = "1958", ImdbId = "tt0157135", Type = "movie" }
+                };
+        }
+
+        [System.Web.Http.HttpPost]
+        public void SaveRating(string movieId, int rating)
+        {
+
+        }
+    }
+
+
+    public class MovieTip
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string ImdbId { get; set; }
+        public string Type { get; set; }
+    }
+}
--- KarinsFilmer/Scripts/controllers/rate-controller.js
+++ KarinsFilmer/Scripts/controllers/rate-controller.js
@@ -21,32 +21,31 @@
         //    });
         //};
 
-        //$scope.saveRating = function (movie) {
-        //    $scope.working = true;
+        $scope.saveRating = function (movie) {
+            $scope.working = true;
 
-        //    $http.post("/api/save", { 'movieId': movie.questionId, 'rating': movie.rating }).success(function (data, status, headers, config) {
-        //        $scope.working = false;
-        //    }).error(function (data, status, headers, config) {
-        //        $scope.title = "Oops... something went wrong";
-        //        $scope.working = false;
-        //    });
-        //};
+            $http.post("/api/MovieApi", { 'movieId': movie.imdbID, 'rating': movie.rating }).success(function (data, status, headers, config) {
+                $scope.working = false;
+            }).error(function (data, status, headers, config) {
+                $scope.title = "Oops... something went wrong";
+                $scope.working = false;
+            });
+        };
 
-        $scope.getMovieTips = function() {
-            $scope.movieTips = {
-                "movieTips": [
-                    { "Title": "Django Unchained", "Year": "2012", "imdbID": "tt1853728", "Type": "movie" },
-                    { "Title": "Django Unchained", "Year": "2012", "imdbID": "tt1853728", "Type": "movie" },
-                    { "Title": "Hercules Unchained", "Year": "1959", "imdbID": "tt0052782", "Type": "movie" },
-                    { "Title": "America Unchained", "Year": "2007", "imdbID": "tt1153075", "Type": "movie" },
-                    { "Title": "Angel Unchained", "Year": "1970", "imdbID": "tt0065401", "Type": "movie" },
-                    { "Title": "Unchained Memories: Readings from the Slave Narratives", "Year": "2003", "imdbID": "tt0343129", "Type": "movie" },
-                    { "Title": "Women Unchained", "Year": "1974", "imdbID": "tt0204742", "Type": "movie" },
-                    { "Title": "Conan Unchained: The Making of 'Conan'", "Year": "2000", "imdbID": "tt0430950", "Type": "movie" },
-                    { "Title": "Unchained", "Year": "1955", "imdbID": "tt0048762", "Type": "movie" },
-                    { "Title": "The Unchained Goddess", "Year": "1958", "imdbID": "tt0157135", "Type": "movie" }
-                ]
-            };
-            console.log($scope.movieTips);
+        $scope.getMovieTips = function () {
+            $scope.working = true;
+            $scope.movieTips = [];
+
+            $http.get("/api/MovieApi")
+                .success(function(data, status, headers, config) {
+                    console.log(data);
+                    $scope.movieTips = data;
+                    $scope.working = false;
+                    console.log(data);
+                })
+                .error(function (data, status, headers, config) {
+                    $scope.title = "Oops... something went wrong";
+                    $scope.working = false;
+                });
         };
     });
